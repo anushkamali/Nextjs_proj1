33d5450ae68eea7e3ba2b8858d8804910bad57fb
anushkamali <anushka.mali@launchventures.co>
2022-09-08 15:54:32 +0530

    initial

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+const About = () => {
+    return ( 
+        <>
+        <Head>
+            <title>Ninja List | About</title>
+            <meta name="keywords" content="ninjas" />
+        </Head>
+        <div>
+            <h1>About</h1>
+            <p>Some para in about.Some para in about.Some para in about.Some para in about.Some para in about.</p>
+            <p>Some para in about.Some para in about.Some para in about.Some para in about.Some para in about.</p>
+        </div>
+        </>
+     );
+}
+ 
+export default About;
